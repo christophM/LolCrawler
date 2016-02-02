d5842fc23ea806df5780cab2355d68cde537d80e
Christoph Molnar <christoph.molnar@googlemail.com>
2016-02-02 21:31:49 +0100

    crawl relevant regions

--- crawl-top-matches.py
+++ crawl-top-matches.py
@@ -1,7 +1,7 @@
 from lolcrawler.rito import RitoAPI
 from config import config
 from pymongo import MongoClient
-from lolcrawler.lolcrawler import ChallengerLolCrawler
+from lolcrawler.lolcrawler import TopLolCrawler
 from datetime import datetime
 
 SEASON="SEASON2016"
@@ -28,7 +28,8 @@ api = RitoAPI(config['api_key'], time_between_requests, region=REGION)
 crawler = TopLolCrawler(api,db_client=db)
 
 
-crawler.start(begin_time=datetime(2016, 1, 31), regions=['kr'])
+crawler.start(begin_time=datetime(2016, 1, 1),
+              regions=['euw', 'na', 'eune', 'kr'])
 
 
 
