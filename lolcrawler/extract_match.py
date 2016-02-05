8ca55e96bb3dca1d4ec6a7454648917591aed659
Christoph Molnar <christoph.molnar@googlemail.com>
2016-02-05 11:20:35 +0100

    add surrender extraction

--- lolcrawler/extract_match.py
+++ lolcrawler/extract_match.py
@@ -11,14 +11,35 @@ TIER_ORDER = {"CHALLENGER": 7,
               "BRONZE": 1,
               "UNRANKED": 0}
 
-def surrendered():
+def surrendered(match):
     '''Checks if the match was surrendered  by one of the teams'''
-    ## Check if Nexus turrets killed and position of players?
-    pass
+    ## Improvement suggestion: Use match timeline if available
+    ## Improvement: Build prediction model based on labeled matches
+
+    match_duration = match['matchDuration']
+
+    ## Cannot surrender before Minute 20
+    if match_duration < 60 * 20:
+        return 0
+    ## Guess surrender by team stats
+    winner_stats = list(filter(lambda x: x['winner'], match['teams']))[0]
+    ## If less than 5 towers have been destroyed by winner, it is a surrender
+    if winner_stats['towerKills'] < 5:
+        return 1
+    ## Cannot win a game without inhibitor kills
+    if winner_stats['inhibitorKills'] == 0:
+        return 1
+    ## Matches that ended after minute 20 are more likely to be surrendered matches. Based on gut feeling.
+    if (match_duration  > 60 * 20) and (match_duration < 60 * 21):
+        return 0.5
+    return 0.05
+
 
 def surrendered_at_20(match):
     '''Checks if the match was surrendered at 20 by one of the teams'''
-    pass
+    match_duration = match['matchDuration']
+    ended_at_20 = ((match_duration  > 60 * 20) and (match_duration < 60 * 21))
+    return ended_at_20 * surrendered(match)
 
 
 def get_highest_tier(tiers_list):
@@ -57,6 +78,8 @@ def extract_match_infos(match):
     tiers = [x["highestAchievedSeasonTier"] for x in match["participants"]]
     extractions["highestPlayerTier"] = get_highest_tier(tiers)
     extractions["lowestPlayerTier"] = get_lowest_tier(tiers)
+    extractions['surrendered'] = surrendered(match)
+    extractions['surrenderedAt20'] = surrendered_at_20(match)
     return extractions
 
 
