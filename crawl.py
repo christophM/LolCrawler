70dde3eefed310fb43dd5a5e351f60146c77c312
Christoph Molnar <christoph.molnar@googlemail.com>
2016-03-04 12:28:11 +0100

    only crawl challeger by default

--- crawl.py
+++ crawl.py
@@ -32,7 +32,7 @@ if __name__=="__main__":
     if action=="top":
         yesterday = date.today() - timedelta(1)
         crawler = TopLolCrawler(api,db_client=db, include_timeline=config["include_timeline"])
-        crawler.start(regions=['euw', 'na', 'kr'])
+        crawler.start(regions=['euw', 'na', 'kr', 'eune'], leagues=['challenger'])
     else:
         ## Initialise crawler
         crawler =  LolCrawler(api, db_client=db, include_timeline=config["include_timeline"])
